21bee6c2bc40df1cb7b2810c65e5eaf357c039a5
Gurkan UCAR <ucar.gurkan@hotmail.com>
2025-04-26 10:32:23 +0300

    project initialized

---
+++ src/test/java/com/gucardev/wallet/WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
